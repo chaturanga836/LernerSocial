d2fd0add4482755b8c7267b2c7343c4570c781fc
chaturanga836 <chaturanga836@gmail.com>
2015-07-21 12:52:54 +0530

    front-page

---
+++ index.php
@@ -0,0 +1,194 @@
+
+<!DOCTYPE>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+	<link rel="stylesheet" href="http://localhost/LernerSocial/bootstrap/css/bootstrap-theme.css"
+	type="text/css" media="screen" />
+	<link rel="stylesheet" href="http://localhost/LernerSocial/bootstrap/css/bootstrap.min.css"
+	type="text/css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="http://localhost/LernerSocial/css/edit.bootstrap.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type="text/javascript" src="http://localhost/LernerSocial/bootstrap/js/bootstrap.js"></script>
+
+	<title>Lerner Social</title>
+
+
+
+</head>
+
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">
+					<h2 class="title">LERNER<small>Social </small></h2>
+				</a>
+			</div>
+			<form class="navbar-form navbar-right" role="search">
+				<div class="form-group">
+					<div class="row">
+						<div class="col-xs-6"><span class="login">User Name</span></div>
+						<div class="col-xs-6"><span class="login">Password</span></div>
+					</div>
+					<div class="row">
+						<div class="col-xs-6">
+							<div class="input-group">
+								<div class="input-group-addon"><i class="glyphicon glyphicon-user"></i></div>
+								<input type="text" class="form-control">
+							</div>
+
+						</div>
+
+						<div class="col-xs-6">
+							<div class="input-group">
+								<div class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></div>
+								<input type="text" class="form-control">
+							</div>
+
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-xs-6"><input type="checkbox"> <span class="login-small">keep me Login</span></div>
+						<div class="col-xs-6"><span class="login-small">Fogot your Password</span></div>
+					</div>	
+				</div>
+				<button type="submit" class="btn btn-success">Login</button>
+			</form>
+		</div>
+	</nav>
+
+
+	<div class="col-xs-6">
+
+		<div class="row">
+			<div class="site-para-openSans-Light">
+				Lorem ipsum dolor sit amet, cu vix alii lucilius. At pri eirmod oblique, 
+				quo an copiosae democritum comprehensam, quodsi assentior usu no. In semper pertinax signiferumque vix.
+				Case primis iracundia no eam, ea eum voluptua expetendis, tota modus nostrum no mei. 
+				Eos atqui graeci cetero ex, scripserit scribentur liberavisse eos in. Eos in illud quidam meliore.
+				Eu everti vituperatoribus mei, ea sensibus vulputate constituam sea.
+			</div>
+		</div>
+		<div class="row">
+			<img src="Image/front_image.jpg">
+		</div>
+		
+	</div>
+
+	<div class="col-xs-6">
+
+		<div class="panel panel-default">
+			<div class="panel-heading"><h2 class="text-center title-green">Sign Up</h2></div>
+			<div class="panel-body">
+				<form>
+					<div class="row">
+						<div class="col-xs-6"><input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email"></div>
+						<div class="col-xs-6"><input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></div>
+					</div><br/>
+					<div class="row">
+						<div class="col-xs-12">
+							<input type="email" class="form-control" id="exampleInputPassword1" placeholder="Email">
+						</div>
+					</div><br/>
+
+					<div class="row">
+						<div class="col-xs-12">
+							<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+						</div>
+					</div><br/>
+
+					<div class="row">
+						<div class="col-xs-12">
+							<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password">
+						</div>
+					</div><br/>
+
+					<div class="row">
+						<div class="col-xs-3"><b>Birthday</b></div>
+						<div class="col-xs-3">
+							<select class="form-control">
+								<option>Month</option>
+								<option>2</option>
+
+							</select>
+						</div>
+						<div class="col-xs-3">
+							<select class="form-control">
+								<option>Day</option>
+								<option>2</option>
+
+							</select>
+						</div>
+						<div class="col-xs-3">
+							<select class="form-control">
+								<option>Year</option>
+								<option>2</option>
+
+							</select>
+						</div>
+					</div><br/>
+
+					<div class="row">
+						<div class="col-xs-3"><b>Gender</b></div>
+						<div class="col-xs-3">
+							<div class="radio">
+								<label>
+									<input type="radio" name="gender" value="" checked>
+									Female
+								</label>
+							</div>
+						</div>
+						<div class="col-xs-3">
+							<div class="radio">
+								<label>
+									<input type="radio" name="gender" value="">
+									Male
+								</label>
+							</div>
+						</div>
+						<div class="col-xs-3"></div>
+					</div><br/>
+					<div class="row">
+						<div class="col-xs-8">
+							<span>By Clicking Sign up, You agreed lerner social's</span><br/>
+							<span>
+								<a href="#" class="title-green">User agreement</a>,
+								<a href="#" class="title-green">private policy</a> and 
+								<a href="#" class="title-green">cookie policy</a>
+							</span>
+						</div>
+						<div class="col-xs-4">
+							<button type="submit" class="btn btn-success">Sign up</button>
+						</div>
+					</div>
+
+				</form>
+			</div>
+		</div>
+
+	</div>
+	
+	<nav>
+		<div class="nav-bottom-inverse">
+			<ul class="nav-bottom nav-bottom-right">
+				<li><a href="#">Terms and Conditions</a></li>
+				<li><a href="#">Contact us</a></li>
+				<li><a href="#">Media</a></li>
+				<li><a href="#">Legal</a></li>
+				<li><a href="#">Feedback</a></li>
+			</ul>
+
+			<ul class="nav-bottom nav-bottom-left">
+				<li><a href="#">Copyright 2015</a></li>
+			</ul>
+			
+		</div>
+	</nav>
+
+</body>
+</html>
